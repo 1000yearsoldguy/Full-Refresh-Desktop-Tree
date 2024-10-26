dfb37b4b1f0fc786eb6bf9fbd613d82f7f430dc1
Nafiz <1000yearsoldguy@gmail.com>
2024-10-27 02:51:59 +0600

    uploading

---
+++ Full fresh.bat
@@ -0,0 +1,19 @@
+@echo off
+DEL /Q "C:\Windows\Temp"
+DEL /Q "C:\Users\1000y\AppData\Local\Temp"
+DEL /Q "C:\Windows\Prefetch"
+
+A:
+tree
+
+B:
+tree
+
+C:
+tree
+
+D:
+tree
+
+C:\Windows\System32
+tree
